b8d86efef3ecf0b23180cb27125e631e9dc0c0a4
ulises933 <93222930+ulises933@users.noreply.github.com>
2023-03-31 16:37:13 -0600

    Update authController.js
    version 3 cambios 1 nissan

--- controllers/authController.js
+++ controllers/authController.js
@@ -14,15 +14,42 @@ exports.login = async (req, res) => {
 
   const user = await User.findOne({ ParticipanteName: req.body.ParticipanteName });
   if (!user) {
-    return res.status(400).send({ success: false, errors: [{ code: "100", title: "Error de autenticación", detail: "Usuario no encontrado" }] });
+      return res.status(401).send({
+          success: false,
+          errors: [{
+              code: "401",
+              "source": "Authentication",
+              "title": "Unauthorized",
+              detail: "User invalid"
+          }]
+      });
   }
 
   const validPassword = req.body.Password === user.Password;
   if (!validPassword) {
-    return res.status(400).send({ success: false, errors: [{ code: "101", title: "Error de autenticación", detail: "Contraseña incorrecta" }] });
+      return res.status(401).send({
+          success: false,
+          errors: [{
+              code: "401",
+              "source": "Authentication",
+              "title": "Unauthorized",
+              detail: "Password invalid"
+          }]
+      });
   }
   
 
   const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-  res.send({ success: true, data: { token } });
+    res.send({
+        success: true,
+        data: { token },
+           "errors": [
+            {
+                "code": "",
+                "source": "",
+                "title": "",
+                "detail": "Sin errores"
+            }
+        ]
+    });
 };
